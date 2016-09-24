0af3fffb9bcd833a88e3c2a6a61c1441be02cf39
artisan1 <513867030@qq.com>
2016-09-24 16:02:47 +0800

    更新。

--- grails-app/controllers/flying2/AccountController.groovy
+++ grails-app/controllers/flying2/AccountController.groovy
@@ -9,7 +9,7 @@ class AccountController {
     def accountService
 
     def login() {
-        //test1111
+        //test2222
         def m = [:]
         if (params.username != null && params.pwd != null) {
             params.pwd = MD5.encode(params.pwd)
