b1a1e1d954992ff9f1f7e5803ff9f6376d8c5e05
nick-running <tjtj54tj@gmail.com>
2016-09-28 23:56:42 +0800

    更新。

--- grails-app/domain/flying2/UserHomeShow.groovy
+++ grails-app/domain/flying2/UserHomeShow.groovy
@@ -3,7 +3,7 @@ package flying2
 class UserHomeShow {
     Date createDate = new Date()
 
-    static belongsTo = [product: Product]
+    static belongsTo = [product: Product, user: User]
     static constraints = {
     }
 }
